a065cae986eabc777c0b032bcfbb8cfdc67e5d84
sandpiturtle <sandpiturtle@mail.ru>
2017-01-04 02:03:26 +0300

    Migrate to sqlite

--- file_engine/models.py
+++ file_engine/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
+    # The files will be automatically uploaded to MEDIA_ROOT/documents/
     document = models.FileField(upload_to='documents/%Y/%m/%d/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
