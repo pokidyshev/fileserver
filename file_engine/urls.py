3e52bd0eb725583e5f9b37b44f16dba57a2f5748
sandpiturtle <sandpiturtle@mail.ru>
2017-01-05 00:11:23 +0300

    Add download

--- file_engine/urls.py
+++ file_engine/urls.py
@@ -5,6 +5,7 @@ from file_engine import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^[0-9a-zA-Z]+$', views.details, name='details'),
     url(r'^upload/$', views.upload, name='upload'),
-    url(r'^[0-9a-zA-Z]+$', views.details, name='details')
+    url(r'^download/[0-9a-zA-Z]+$', views.download, name='download'),
 ]
--- file_engine/views.py
+++ file_engine/views.py
@@ -1,4 +1,7 @@
+import os
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.conf import settings
 
 from .models import Document
 from .forms import DocumentForm
@@ -8,7 +11,7 @@ from .encoding import encode, decode
 def home(request):
     documents = Document.objects.all()
     return render(request, 'file_engine/home.html', {
-        'documents': documents
+        'documents': documents,
     })
 
 
@@ -22,7 +25,7 @@ def upload(request):
         form = DocumentForm()
 
     return render(request, 'file_engine/upload.html', {
-        'form': form
+        'form': form,
     })
 
 
@@ -30,5 +33,24 @@ def details(request):
     encoded_id = request.path.split('/')[-1]
     obj = get_object_or_404(Document, pk=decode(encoded_id))
     return render(request, 'file_engine/details.html', {
-        'obj': obj
+        'obj': obj,
+        'encoded_id': encoded_id,
     })
+
+
+def download(request):
+    encoded_id = request.path.split('/')[-1]
+    obj = get_object_or_404(Document, pk=decode(encoded_id))
+    file_path = os.path.join(settings.MEDIA_ROOT, obj.document.name)
+
+    if not os.path.exists(file_path):
+        raise Http404
+
+    with open(file_path, 'rb') as fh:
+        response = HttpResponse(
+            fh.read(),
+            content_type='application/force-download'
+        )
+        response['Content-Disposition'] = ('inline; filename=' +
+                                           os.path.basename(file_path))
+        return response
